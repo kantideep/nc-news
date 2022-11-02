bc6c5bada87008909750ed37399ed6a16f90412d
Kantideep Thota <kantideep@gmail.com>
2022-11-02 16:54:40 +0000

    Vote on an article refactored, but vote count doesn't update unless refeshed

--- src/components/SingleArticle.jsx
+++ src/components/SingleArticle.jsx
@@ -1,40 +1,56 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { fetchArticleById, updateArticleVotes } from "../fetchAPI";
+import ErrorPage from "./ErrorPage";
 
 
 const SingleArticle = () => {
   const { article_id } = useParams();
-  const [article, setArticle] = useState({});
   const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [article, setArticle] = useState([]);
+  const { title, topic, body, author, created_at } = article;
   const [votes, setVotes] = useState(0);
 
+  const handleClick = (vote) => {
+        updateArticleVotes(article_id, vote)
+        .then(() => {
+            setArticle((article) => {
+                article.votes = 1 + vote;
+                return article;
+            })
+        })
+ }
+  
   useEffect(() => {
     setIsLoading(true);
-    fetchArticleById(article_id).then((article) => {
-      setArticle(article);
-      setIsLoading(false);
+    fetchArticleById(article_id)
+      .then((article) => {
+        setVotes(article.votes);
+        setArticle(article);
+        setIsLoading(false);
     })
+      .catch((err) => {
+        setIsLoading(false);
+        setError(err);
+      });
   }, [article_id]);
 
-  const handleClick = () => {
-    setVotes((currVotes) => currVotes + 1);
-     return updateArticleVotes({ inc_votes: 1 })
-   };
-  
-    if (isLoading) return (<h2>Loading article...</h2>);
+   
+  if (isLoading) return (<h2>Loading article...</h2>);
+
+  if(error) return (<ErrorPage/>)
 
     return (
       <main>
         <section>
-          <h2>{article.title}</h2>
-          <h3>Author: {article.author}</h3>
-          <h3>Topic: {article.topic}</h3>
-          <p>{article.body}</p>
-          <h4>Date: {article.created_at}</h4>
-          {/* <h5>Votes: {article.votes}</h5> */}
+          <h2>{title}</h2>
+          <h3>Author: {author}</h3>
+          <h3>Topic: {topic}</h3>
+          <p>{body}</p>
+          <h4>Date: {created_at}</h4>
           <div>
-            <button onClick={handleClick}>👍</button>
+            <button onClick={() => handleClick(1)}>👍</button>
             <h5>Votes: {votes}</h5>
           </div>
         </section>
